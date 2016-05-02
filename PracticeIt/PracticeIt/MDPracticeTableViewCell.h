aa6e67ac5623b7b1c1dc4f5bf7dc0473f282e37d
Matheus Martins <matheusmcrds@gmail.com>
2016-05-02 11:57:46 -0300

    Telas ListOfPracticesView, AddPracticeView com algums scripts.

---
+++ PracticeIt/PracticeIt/MDPracticeTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  MDPracticeTableViewCell.h
+//  PracticeIt
+//
+//  Created by bepid on 5/2/16.
+//  Copyright © 2016 MatheusDaniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDPracticeTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
