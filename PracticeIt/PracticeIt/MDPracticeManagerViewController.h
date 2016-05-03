4c876f01b2328ae62c96a1eea1d452c61a9635db
Matheus Martins <matheusmcrds@gmail.com>
2016-05-03 11:59:43 -0300

    Tela ManagePractice e perfumariazinhas a mais

---
+++ PracticeIt/PracticeIt/MDPracticeManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDPracticeManagerViewController.h
+//  PracticeIt
+//
+//  Created by bepid on 5/3/16.
+//  Copyright © 2016 MatheusDaniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDPracticeManagerViewController : UIViewController
+
+@end
---
+++ PracticeIt/PracticeIt/MDTaskTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MDTaskTableViewCell.m
+//  PracticeIt
+//
+//  Created by bepid on 5/3/16.
+//  Copyright © 2016 MatheusDaniel. All rights reserved.
+//
+
+#import "MDTaskTableViewCell.h"
+
+@implementation MDTaskTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
