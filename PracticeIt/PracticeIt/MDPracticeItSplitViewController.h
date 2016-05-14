1118aab1b522665065bf446fb71951cffb726c72
Meu Nome <Meu emaile>
2016-05-14 13:43:37 -0300

    icons

---
+++ PracticeIt/PracticeIt/MDPracticeItSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDPracticeItSplitViewController.h
+//  PracticeIt
+//
+//  Created by bepid on 5/14/16.
+//  Copyright © 2016 MatheusDaniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDPracticeItSplitViewController : UISplitViewController
+
+@end
