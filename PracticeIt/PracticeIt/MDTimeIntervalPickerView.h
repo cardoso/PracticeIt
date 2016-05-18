1ff5b1999072411dcf7126f546e8a3da0fbd45f4
Matheus Martins <matheusmcrds@gmail.com>
2016-05-18 11:51:31 -0300

    Mudadas as horas e minutos para minutos e segundos. Agora as tarefas só podem ter menos de 1 hora.

--- PracticeIt/PracticeIt/MDTimeIntervalPickerView.h
+++ PracticeIt/PracticeIt/MDTimeIntervalPickerView.h
@@ -10,6 +10,28 @@
 
 @interface MDTimeIntervalPickerView : UIPickerView <UIPickerViewDataSource, UIPickerViewDelegate>
 
+@property NSTimeInterval defaultTimeInterval;
+
+-(instancetype)init;
+
+-(instancetype)initWithFrame:(CGRect)frame;
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder;
+
 -(NSTimeInterval)timeInterval;
+
+-(BOOL)setTimeInterval:(NSTimeInterval)timeInterval animated:(BOOL)animated;
+
+-(BOOL)setSeconds:(NSInteger)seconds animated:(BOOL)animated;
+
+-(BOOL)setMinutes:(NSInteger)minutes animated:(BOOL)animated;
+
+-(NSInteger)secondsComponent;
+
+-(NSInteger)minutesComponent;
+
+-(NSInteger)seconds;
+
+-(NSInteger)minutes;
 
 @end
